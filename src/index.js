79c585ec6b65b583246451ed1c26aab39b845264
Om Thakare <omthakare1606@gmail.com>
2023-06-07 17:09:58 +0530

    added hook: useUser

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export * from './hooks/useEasyauth/useEasyauth.jsx';
 export * from './components/UserProfile/UserProfile.jsx';
 export * from './components/SignedInAndSignedOut/SignedIn.jsx';
 export * from './components/SignedInAndSignedOut/SignedOut.jsx';
+export * from './hooks/useUser/useUser.jsx';
